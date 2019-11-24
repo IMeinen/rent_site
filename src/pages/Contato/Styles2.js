959b2095923fbd514ffad5bf34e6d36696133dd7
arturmeinen <arturmeinen@gmail.com>
2019-11-24 01:22:04 -0300

    alterações contato6

--- src/pages/Contato/Styles2.js
+++ src/pages/Contato/Styles2.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Footer = styled.footer`
   @import url('https://fonts.googleapis.com/css?family=Be+Vietnam:100&display=swap');
 
-  background: linear-gradient(to right, #aaaa2d, #f7532c);
+  background: linear-gradient(to right, #fda22d, #f7532c);
   width: 100%;
   position: relative;
   z-index: 3;
@@ -26,7 +26,7 @@ export const FirstSection = styled.section`
     flex-direction: column;
     margin-top: 2%;
     box-sizing: border-box;
-    transform: translateX(10%);
+    transform: translateX(20%);
     input {
       border-left: none;
       border-top: none;
