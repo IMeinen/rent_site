61aced026edb6ce231070e6b3779540a27bc114d
arturmeinen <arturmeinen@gmail.com>
2019-11-24 01:10:04 -0300

    alterações contato2

--- src/pages/Contato/Contato.js
+++ src/pages/Contato/Contato.js
@@ -74,6 +74,7 @@ export default function Main() {
           <input name="Nome" placeholder="Nome completo" />
           <input name="Email" placeholder="Email" />
           <input name="Phone" placeholder="Telefone" />
+          <MyRadioButtonAcademia />
         </form>
       </FirstSection>
     </Footer>
